bea00083886318adbbbe6156bc6f8e5190000f96
Андрей Захаров <andryushka.zakharov.04@mail.ru>
2023-05-12 22:07:23 +0700

    Creating multiple API methods

---
+++ Backend/src/FunnyCode/Controllers/ProfileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FunnyCode.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class ProfileController: ControllerBase
+{
+    [HttpGet("{id:Guid}")]
+    public IActionResult GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id:Guid}")]
+    public IActionResult Update(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id:Guid}")]
+    public IActionResult Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
