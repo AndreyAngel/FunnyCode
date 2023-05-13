bd759118b6ecfc2ce8695aa611ee8ed48b3d860a
xtitaniumx <artemspasenko@gmail.com>
2023-05-13 23:37:47 +0700

    add request to project and user profile repositories

--- Mobile/Android/data/src/main/java/com/example/funnyapp/data/repository/ProjectRepository.kt
+++ Mobile/Android/data/src/main/java/com/example/funnyapp/data/repository/ProjectRepository.kt
@@ -2,19 +2,34 @@ package com.example.funnyapp.data.repository
 
 import android.content.Context
 import com.example.funnyapp.data.api.MainApiClient
-import com.example.funnyapp.data.model.request.GetProjectByTeamIdParam
+import com.example.funnyapp.data.model.request.GetProjectsByTeamIdParam
 import com.example.funnyapp.data.model.request.GetProjectsByDivisionId
+import com.example.funnyapp.data.model.request.GetProjectsByIdParam
+import com.example.funnyapp.data.model.request.GetProjectsBySubdivisionIdParam
+import com.example.funnyapp.data.model.request.GetProjectsByUserIdParam
 
 class ProjectRepository(context: Context, apiClient: MainApiClient) : BaseRepository(context, apiClient) {
     fun getAllProjects() = apiRequestFlow {
         apiService.getAllProjects()
     }
 
-    fun getProjectByTeamId(getProjectByTeamIdParam: GetProjectByTeamIdParam) = apiRequestFlow {
-        apiService.getProjectById(teamId = getProjectByTeamIdParam.teamId)
+    fun getProjectsByUserId(getProjectsByUserIdParam: GetProjectsByUserIdParam) = apiRequestFlow {
+        apiService.getProjectsByUserId(userId = getProjectsByUserIdParam.userId)
+    }
+
+    fun getProjectsByTeamId(getProjectsByTeamIdParam: GetProjectsByTeamIdParam) = apiRequestFlow {
+        apiService.getProjectsByTeamId(teamId = getProjectsByTeamIdParam.teamId)
+    }
+
+    fun getProjectsBySubdivisionId(getProjectsBySubdivisionIdParam: GetProjectsBySubdivisionIdParam) = apiRequestFlow {
+        apiService.getProjectsBySubdivisionId(subdivisionId = getProjectsBySubdivisionIdParam.subdivisionId)
     }
 
     fun getProjectsByDivisionId(getProjectsByDivisionId: GetProjectsByDivisionId) = apiRequestFlow {
-        apiService.getProjectsById(divisionId = getProjectsByDivisionId.divisionId)
+        apiService.getProjectsByDivisionId(divisionId = getProjectsByDivisionId.divisionId)
+    }
+
+    fun getProjectsById(getProjectsByIdParam: GetProjectsByIdParam) = apiRequestFlow {
+        apiService.getProjectsById(projectId = getProjectsByIdParam.projectId)
     }
 }
--- Mobile/Android/data/src/main/java/com/example/funnyapp/data/repository/UserProfileRepository.kt
+++ Mobile/Android/data/src/main/java/com/example/funnyapp/data/repository/UserProfileRepository.kt
@@ -2,13 +2,17 @@ package com.example.funnyapp.data.repository
 
 import android.content.Context
 import com.example.funnyapp.data.api.MainApiClient
-import com.example.funnyapp.data.model.request.DeleteUserProfileParam
+import com.example.funnyapp.data.model.request.GetProfileByFiltersParam
 import com.example.funnyapp.data.model.request.GetUserProfileParam
 import com.example.funnyapp.data.model.request.UpdateUserProfileParam
 
 class UserProfileRepository(context: Context, apiClient: MainApiClient) : BaseRepository(context, apiClient) {
     fun getProfile(getUserProfileParam: GetUserProfileParam) = apiRequestFlow {
         apiService.getProfileById(id = getUserProfileParam.id)
+    }
+
+    fun getProfileByFilters(getProfileByFiltersParam: GetProfileByFiltersParam) = apiRequestFlow {
+        apiService.getProfileByFilters(request = getProfileByFiltersParam)
     }
 
     fun updateProfile(updateUserProfileParam: UpdateUserProfileParam) = apiRequestFlow {
