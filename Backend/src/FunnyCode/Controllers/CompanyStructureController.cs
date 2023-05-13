d1b1dff960bd96ac251524bc2978248c092d8a49
Андрей Захаров <andryushka.zakharov.04@mail.ru>
2023-05-13 12:13:04 +0700

    Adding Identity

--- Backend/src/FunnyCode/Controllers/CompanyStructureController.cs
+++ Backend/src/FunnyCode/Controllers/CompanyStructureController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FunnyCode.Domain.Core.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace FunnyCode.Controllers;
 
@@ -6,54 +9,105 @@ namespace FunnyCode.Controllers;
 [ApiController]
 public class CompanyStructureController : ControllerBase
 {
+    /// <summary>
+    /// Get all divisions
+    /// </summary>
+    /// <returns></returns>
+    /// <response code="200"> Successful completion </response>
+    /// <response code="401"> Unauthorized </response>
     [HttpGet]
+    [Authorize(Policy = "ChangingOfCatalog")]
+    [ProducesResponseType(typeof(), (int)HttpStatusCode.Created)]
+    [ProducesResponseType(typeof(string), (int)HttpStatusCode.Conflict)]
+    [ProducesResponseType(typeof(string), (int)HttpStatusCode.NotFound)]
     public IActionResult GetDivisions()
     {
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Get division by Id
+    /// </summary>
+    /// <param name="id"> Division Id </param>
+    /// <returns></returns>
     [HttpGet("{id:Guid}")]
     public IActionResult GetDivisionById(Guid id)
     {
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Get division by name
+    /// </summary>
+    /// <param name="name"> Division name </param>
+    /// <returns></returns>
     [HttpGet("{name}")]
     public IActionResult GetDivisionByName(string name)
     {
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Get all subdivisions by division Id
+    /// </summary>
+    /// <param name="divisionId"> Division Id </param>
+    /// <returns></returns>
     [HttpGet("{divisionId:Guid}")]
-    public IActionResult GetSubdivisions(Guid divisionsId)
+    public IActionResult GetSubdivisions(Guid divisionId)
     {
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Get subdivision by Id
+    /// </summary>
+    /// <param name="id"> Subdivision Id </param>
+    /// <returns></returns>
     [HttpGet("{id:Guid}")]
     public IActionResult GetSubdivisionById(Guid id)
     {
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Get subdivision by name
+    /// </summary>
+    /// <param name="name"> Subdivision name </param>
+    /// <returns></returns>
     [HttpGet("{name}")]
     public IActionResult GetSubdivisionByName(string name)
     {
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Get all teams by subdivision Id
+    /// </summary>
+    /// <param name="subdivisionId"> Subdivision Id </param>
+    /// <returns></returns>
     [HttpGet("{subdivisionId:Guid}")]
-    public IActionResult GetTeams(Guid subdivisionsId)
+    public IActionResult GetTeams(Guid subdivisionId)
     {
         throw new NotImplementedException();
     }
 
+
+    /// <summary>
+    /// Get team by Id
+    /// </summary>
+    /// <param name="id"> Team Id </param>
+    /// <returns></returns>
     [HttpGet("{id:Guid}")]
     public IActionResult GetTeamById(Guid id)
     {
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Get team by name
+    /// </summary>
+    /// <param name="name"> Team name </param>
+    /// <returns></returns>
     [HttpGet("{name}")]
     public IActionResult GetTeamByName(string name)
     {
---
+++ Backend/src/FunnyCode/Helpers/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace FunnyCode.Helpers;
+
+/// <summary>
+/// Class for models mapping
+/// </summary>
+public class MappingProfile : Profile
+{
+    /// <summary>
+    /// Initializes a new instance of <see cref="MappingProfile"/>.
+    /// </summary>
+    public MappingProfile()
+    {
+
+    }
+}
